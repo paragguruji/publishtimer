26d9266b7cba1aa6d43615f1cfc21281275895ef
Parag Guruji <paragguruji@gmail.com>
2016-04-28 14:54:51 +0530

    comments added in custom_exceptions.py
    Comments explaining the purpose and usage of custom exception code 512
    added

--- publishtimer/custom_exceptions.py
+++ publishtimer/custom_exceptions.py
@@ -8,7 +8,9 @@ Created on Wed Apr 27 19:05:53 2016
 from werkzeug.exceptions import HTTPException
 
 class WriteScheduleFailedError(HTTPException):
-
+    """Exception to be raised when schedule is computed successfully by the publishtimer but save_schedule API returns failure response.
+        The response includes error code and reason as returned by the save_schedule API and the schedule computed by publishtimer
+    """
     def __init__(self, **kwargs):
         self.code = 512
         self.upstream_response = kwargs.get('upstream_response')
